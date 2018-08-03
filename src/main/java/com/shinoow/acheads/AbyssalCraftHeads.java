7cc29b5e66374a60286fbec97b81d8ce7f390740
Shinoow <felix@lundquist.ws>
2018-08-03 18:09:19 +0200

    AbyssalCraft Heads 2.0.4
    *Bosses, Coralium Infested Squids and Remnants now drop their respective heads
    *Bump to AbyssalCraft 1.9.4.11

--- src/main/java/com/shinoow/acheads/AbyssalCraftHeads.java
+++ src/main/java/com/shinoow/acheads/AbyssalCraftHeads.java
@@ -44,7 +44,7 @@ import net.minecraftforge.fml.common.event.*;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = AbyssalCraftHeads.modid, name = AbyssalCraftHeads.name, version = AbyssalCraftHeads.version, dependencies = "required-after:forge@[forgeversion,);required-after:abyssalcraft@[1.9.4.9,]", useMetadata = false,
+@Mod(modid = AbyssalCraftHeads.modid, name = AbyssalCraftHeads.name, version = AbyssalCraftHeads.version, dependencies = "required-after:forge@[forgeversion,);required-after:abyssalcraft@[1.9.4.11,];after:thaumcraft", useMetadata = false,
 acceptedMinecraftVersions = "[1.11.2]", updateJSON = "https://raw.githubusercontent.com/Shinoow/AbyssalCraft-Heads/master/version.json", certificateFingerprint = "cert_fingerprint")
 public class AbyssalCraftHeads {
 
@@ -118,10 +118,11 @@ public class AbyssalCraftHeads {
 	public void lootLoad(LootTableLoadEvent event){
 		LootCondition[] conditions = new LootCondition[]{new KilledByPlayer(false), new RandomChanceWithLooting(0.025f, 0.01f)};
 		if(event.getName().equals(ACLoot.ENTITY_ABYSSAL_ZOMBIE))
-			addLootEntry(event.getTable(), conditions, 5, "abyssal_zombie");
+			addLootEntry(event.getTable(), conditions, 0, "abyssal_zombie");
 		if(event.getName().equals(ACLoot.ENTITY_EVIL_PIG))
-			addLootEntry(event.getTable(), conditions, 2, "evil_pig");
-		//J'zahar 2
+			addLootEntry(event.getTable(), conditions, 1, "evil_pig");
+		if(event.getName().equals(ACLoot.ENTITY_JZAHAR))
+			addLootEntry(event.getTable(), conditions, 2, "jzahar");
 		if(event.getName().equals(ACLoot.ENTITY_ABYSSALNITE_GOLEM))
 			addLootEntry(event.getTable(), conditions, 3, "abyssalnite_golem");
 		if(event.getName().equals(ACLoot.ENTITY_DREADED_ABYSSALNITE_GOLEM))
@@ -130,7 +131,8 @@ public class AbyssalCraftHeads {
 			addLootEntry(event.getTable(), conditions, 5, "dreadguard");
 		if(event.getName().equals(ACLoot.ENTITY_SPECTRAL_DRAGON))
 			addLootEntry(event.getTable(), conditions, 6, "spectral_dragon");
-		//Asorah 7
+		if(event.getName().equals(ACLoot.ENTITY_ASORAH))
+			addLootEntry(event.getTable(), conditions, 7, "asorah");
 		if(event.getName().equals(ACLoot.ENTITY_SHADOW_CREATURE))
 			addLootEntry(event.getTable(), conditions, 8, "shadow_creature");
 		if(event.getName().equals(ACLoot.ENTITY_SHADOW_MONSTER))
@@ -141,14 +143,16 @@ public class AbyssalCraftHeads {
 			addLootEntry(event.getTable(), conditions, 11, "dread_spawn");
 		if(event.getName().equals(ACLoot.ENTITY_DEMON_PIG))
 			addLootEntry(event.getTable(), conditions, 12, "demon_pig");
-		//Cha'garoth 13
+		if(event.getName().equals(ACLoot.ENTITY_CHAGAROTH))
+			addLootEntry(event.getTable(), conditions, 13, "chagaroth");
 		if(event.getName().equals(ACLoot.ENTITY_FIST_OF_CHAGAROTH)){
 			addLootEntry(event.getTable(), conditions, 14, "fist_of_chagaroth");
 			addLootEntry(event.getTable(), conditions, 51, "fist_of_chagaroth_left");
 		}
 		if(event.getName().equals(ACLoot.ENTITY_SHADOW_BEAST))
 			addLootEntry(event.getTable(), conditions, 15, "shadow_beast");
-		//Sacthoth 16
+		if(event.getName().equals(ACLoot.ENTITY_SACTHOTH))
+			addLootEntry(event.getTable(), conditions, 16, "sacthoth");
 		if(event.getName().equals(ACLoot.ENTITY_SKELETON_GOLIATH))
 			addLootEntry(event.getTable(), conditions, 17, "skeleton_goliath");
 		if(event.getName().equals(ACLoot.ENTITY_SPAWN_OF_CHAGAROTH))
@@ -177,16 +181,20 @@ public class AbyssalCraftHeads {
 			addLootEntry(event.getTable(), conditions, 29, "anti_zombie");
 		if(event.getName().equals(ACLoot.ENTITY_OMOTHOL_GHOUL))
 			addLootEntry(event.getTable(), conditions, 30, "omothol_ghoul");
-		//TODO individual remnant loot tables
-		if(event.getName().equals(ACLoot.ENTITY_REMNANT)){
+		if(event.getName().equals(ACLoot.ENTITY_REMNANT))
 			addLootEntry(event.getTable(), conditions, 31, "remnant");
+		if(event.getName().equals(ACLoot.ENTITY_REMNANT_BANKER))
 			addLootEntry(event.getTable(), conditions, 32, "remnant_banker");
+		if(event.getName().equals(ACLoot.ENTITY_REMNANT_BLACKSMITH))
 			addLootEntry(event.getTable(), conditions, 33, "remnant_blacksmith");
+		if(event.getName().equals(ACLoot.ENTITY_REMNANT_BUTCHER))
 			addLootEntry(event.getTable(), conditions, 34, "remnant_butcher");
+		if(event.getName().equals(ACLoot.ENTITY_REMNANT_LIBRARIAN))
 			addLootEntry(event.getTable(), conditions, 35, "remnant_librarian");
+		if(event.getName().equals(ACLoot.ENTITY_REMNANT_MASTER_BLACKSMITH))
 			addLootEntry(event.getTable(), conditions, 36, "remnant_master_blacksmith");
+		if(event.getName().equals(ACLoot.ENTITY_REMNANT_PRIEST))
 			addLootEntry(event.getTable(), conditions, 37, "remnant_priest");
-		}
 		if(event.getName().equals(ACLoot.ENTITY_MINION_OF_THE_GATEKEEPER))
 			addLootEntry(event.getTable(), conditions, 38, "minion_of_the_gatekeeper");
 		if(event.getName().equals(ACLoot.ENTITY_LESSER_SHOGGOTH))
@@ -215,6 +223,8 @@ public class AbyssalCraftHeads {
 			addLootEntry(event.getTable(), conditions, 48, "evil_sheep");
 		if(event.getName().equals(ACLoot.ENTITY_DEMON_SHEEP))
 			addLootEntry(event.getTable(), conditions, 49, "demon_sheep");
+		if(event.getName().equals(ACLoot.ENTITY_CORALIUM_INFESTED_SQUID))
+			addLootEntry(event.getTable(), conditions, 50, "coralium_infested_squid");
 	}
 
 	private void addLootEntry(LootTable table, LootCondition[] conditions, int meta, String name) {
